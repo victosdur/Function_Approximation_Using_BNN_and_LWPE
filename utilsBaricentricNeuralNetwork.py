2c7d5122a2b090dfeed14d029f14ff012597ebe0
victosdur <victortoscano21@gmail.com>
2025-07-22 17:18:13 +0200

    Update

--- utilsBaricentricNeuralNetwork.py
+++ utilsBaricentricNeuralNetwork.py
@@ -43,7 +43,6 @@ class BaricentricSigmaLayerTf(Layer):
             output += segment_output
         return output
 
-
 class BaricentricSigmaNetworkTf(Model):
     def __init__(self, points, **kwargs):
         super(BaricentricSigmaNetworkTf, self).__init__(**kwargs)
@@ -51,3 +50,65 @@ class BaricentricSigmaNetworkTf(Model):
 
     def call(self, x):
         return self.layer(x)
+    
+
+
+
+def stepestrella(x):
+    return tf.cast(x > 0, dtype=tf.float32)
+
+class CustomDense(tf.keras.layers.Layer):
+    def __init__(self, weights, biases, activations):
+        super(CustomDense, self).__init__()
+        self.weights = tf.constant(weights, dtype=tf.float32)
+        self.biases = tf.constant(biases, dtype=tf.float32)
+        self.activations = activations  # lista de funciones
+
+    def call(self, inputs):
+        z = tf.matmul(inputs, self.weights)
+        z = tf.add(z, self.biases)
+        if len(self.activations) == 1:
+            return self.activations(z)
+        else:
+            out = tf.stack([self.activations[i](z[:, i]) for i in range(z.shape[1])], axis=1)
+        return out
+    
+class BaricentricNetworkSegment(Model):
+    def __init__(self, a, b, fa, fb, **kwargs):
+        super(BaricentricNetworkSegment, self).__init__(**kwargs)
+        # Layer 1: 4 neurons
+        w1 = [[1, -1, 1, -1]]  # input_dim=1, output_dim=4
+        b1 = [0, 0, -1, 1]
+        act1 = [tf.nn.relu, stepestrella, stepestrella, tf.nn.relu]
+
+        self.layer1 = CustomDense(tf.transpose(w1), b1, act1)
+
+        # Layer 2: 2 neurons
+        w2 = [[-1, 0], [-2, -2], [-2, -2], [0, -1]]  # 4x2
+        b2 = [1.0, 1.1]
+        act2 = tf.nn.relu
+
+        self.layer2 = CustomDense(w2, b2, act2)
+
+        # Layer 3: 1 neuron
+        w3 = [[fa], [fb]]  # 2x1
+        b3 = [0]
+        act3 = tf.identity
+
+        self.layer3 = CustomDense(w3, b3, act3)
+
+    def call(self, x):
+        t = (x - a) / (b - a)
+        x = self.layer1(t)
+        x = self.layer2(t)
+        x = self.layer3(t)
+        return x
+    
+    class SumOfSubNetworks(tf.keras.Model):
+        def __init__(self, num_subnets=3):
+            super(SumOfSubNetworks, self).__init__()
+            self.subnets = [FixedSubNetwork() for _ in range(num_subnets)]
+
+        def call(self, inputs):
+            outputs = [subnet(inputs) for subnet in self.subnets]
+            return tf.add_n(outputs)
